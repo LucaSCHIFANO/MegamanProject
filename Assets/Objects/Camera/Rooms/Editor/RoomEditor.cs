5a8966e2c28f7b52d231d541e81d8e71f3c7c573
Luca Schifano <luca.schifano005@gmail.com>
2024-01-20 11:59:35 +0100

    [ADD] Set transition collider only on precise points

--- Assets/Objects/Camera/Rooms/Editor/RoomEditor.cs
+++ Assets/Objects/Camera/Rooms/Editor/RoomEditor.cs
@@ -1,12 +1,25 @@
 using Codice.Client.Commands;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 [CustomEditor(typeof(Room))]
 public class RoomEditor : Editor
 {
+    SerializedProperty m_HandlesColor;
+    SerializedProperty m_HandlesColliderColor;
+    SerializedProperty m_LineThickness;
+
+    private void OnEnable()
+    {
+        m_HandlesColor = serializedObject.FindProperty("handlesColor");
+        m_HandlesColliderColor = serializedObject.FindProperty("handlesColliderColor");
+        m_LineThickness = serializedObject.FindProperty("lineThickness");
+    }
+
     private void OnSceneGUI()
     {
         var room = target as Room;
@@ -24,16 +37,110 @@ public class RoomEditor : Editor
         var bottomRightLimit = new Vector3(topRightLimit.x, bottomLeftLimit.y, 0);
         var topLeftLimit = new Vector3(bottomLeftLimit.x, topRightLimit.y, 0);
 
-        var lineThickness = room.LineThickness;
-
-        Handles.color = room.HandlesColor;
 
 
-        Handles.DrawLine(bottomLeftLimit, topLeftLimit, lineThickness);
-        Handles.DrawLine(topLeftLimit, topRightLimit, lineThickness);
-        Handles.DrawLine(topRightLimit, bottomRightLimit, lineThickness);
-        Handles.DrawLine(bottomRightLimit, bottomLeftLimit, lineThickness);
+        if (room.DrawDebug)
+        {
+
+            var lineThickness = room.LineThickness;
+
+            Handles.color = room.HandlesColor;
+
+            Handles.DrawLine(bottomLeftLimit, topLeftLimit, lineThickness);
+            Handles.DrawLine(topLeftLimit, topRightLimit, lineThickness);
+            Handles.DrawLine(topRightLimit, bottomRightLimit, lineThickness);
+            Handles.DrawLine(bottomRightLimit, bottomLeftLimit, lineThickness);
+
+
+            if (room.Transitions.Count > 0)
+            {
+                Handles.color = room.HandlesColliderColor;
+
+
+                for (int i = 0; i < room.Transitions.Count; i++)
+                {
+                    var size = room.GetColliderHeightWidth(room.Transitions[i]) / 2;
+                    var centralPosition = room.GetColliderCentralPoint(room.Transitions[i]);
+
+                    var bottomLeftCollider = new Vector2(centralPosition.x - size.x, centralPosition.y - size.y);
+                    var topRightCollider = new Vector2(centralPosition.x + size.x, centralPosition.y + size.y);
+
+                    var bottomRightCollider = new Vector2(centralPosition.x + size.x, centralPosition.y - size.y);
+                    var topLeftCollider = new Vector2(centralPosition.x - size.x, centralPosition.y + size.y);
+
+                    Handles.DrawLine(bottomLeftCollider, topLeftCollider, lineThickness);
+                    Handles.DrawLine(topLeftCollider, topRightCollider, lineThickness);
+                    Handles.DrawLine(topRightCollider, bottomRightCollider, lineThickness);
+                    Handles.DrawLine(bottomRightCollider, bottomLeftCollider, lineThickness);
+
+                }
+
+            }
+        }
 
         PrefabUtility.RecordPrefabInstancePropertyModifications(room);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        var room = target as Room;
+
+        DrawPropertiesExcluding(serializedObject, "handlesColor", "handlesColliderColor", "lineThickness");
+
+        if (room.DrawDebug) 
+        {
+            EditorGUILayout.PropertyField(m_HandlesColor, new GUIContent("Handles Color"), GUILayout.Height(20));
+            EditorGUILayout.PropertyField(m_HandlesColliderColor, new GUIContent("Handles Collider Color"), GUILayout.Height(20));
+            EditorGUILayout.PropertyField(m_LineThickness, new GUIContent("Line Thickness"), GUILayout.Height(20));
+        }
+
+            serializedObject.ApplyModifiedProperties();
+    }
+}
+
+
+
+[CustomPropertyDrawer(typeof(Transition))]
+public class TransitionEditor : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position,label, property);
+
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        var transitionSideRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+        var newRoomIdRect = new Rect(position.x, position.y + 20f, position.width, EditorGUIUtility.singleLineHeight);
+        var isColliderReducedRect = new Rect(position.x, position.y + 50f, position.width, EditorGUIUtility.singleLineHeight);
+        var offsetRect = new Rect(position.x, position.y + 70f, position.width, EditorGUIUtility.singleLineHeight);
+        var sizeRect = new Rect(position.x, position.y + 90f, position.width, EditorGUIUtility.singleLineHeight);
+
+        var transitionSide = property.FindPropertyRelative("transitionSide");
+        var newRoomId = property.FindPropertyRelative("newRoomID");
+        var isColliderReduced = property.FindPropertyRelative("isColliderReduced");
+        var offset = property.FindPropertyRelative("offset");
+        var size = property.FindPropertyRelative("size");
+
+
+        transitionSide.intValue = EditorGUI.Popup(transitionSideRect, "Transition side", transitionSide.intValue, transitionSide.enumNames);
+        EditorGUI.PropertyField(newRoomIdRect, newRoomId);
+        EditorGUI.PropertyField(isColliderReducedRect, isColliderReduced);
+
+        if (isColliderReduced.boolValue)
+        {
+            EditorGUI.PropertyField(offsetRect, offset);
+            EditorGUI.PropertyField(sizeRect, size);
+        }
+
+        EditorGUI.indentLevel = indent;
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        if(property.FindPropertyRelative("isColliderReduced").boolValue)
+            return (20 - EditorGUIUtility.singleLineHeight) + (EditorGUIUtility.singleLineHeight * 6f);
+        else return (20 - EditorGUIUtility.singleLineHeight) + (EditorGUIUtility.singleLineHeight * 4f);
     }
 }
--- Assets/Objects/Camera/Rooms/Room.cs
+++ Assets/Objects/Camera/Rooms/Room.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static Room;
 
+
 public class Room : MonoBehaviour
 {
     public const float gridX = 2.4f;
@@ -36,7 +37,9 @@ public class Room : MonoBehaviour
     }
     
     [Header("Debug")]
-    [SerializeField] private Color handlesColor;
+    [SerializeField] private bool drawDebug;
+    [SerializeField] private Color handlesColor = Color.red;
+    [SerializeField] private Color handlesColliderColor = Color.green;
     [SerializeField] private float lineThickness = 3;
 
     #region GetSet
@@ -56,8 +59,11 @@ public class Room : MonoBehaviour
     public Vector2 WorldTopRightLimit { get => worldTopRightLimit + transform.position;}
 
     //Debug
+    public bool DrawDebug { get => drawDebug; set => drawDebug = value; }
     public Color HandlesColor { get => handlesColor; }
     public float LineThickness { get => lineThickness; }
+    public Color HandlesColliderColor { get => handlesColliderColor; }
+    [SerializeField, HideInInspector] public List<Transition> Transitions { get => transitions;}
 
     #endregion
 
@@ -75,46 +81,19 @@ public class Room : MonoBehaviour
 
     void SetTransitionHitbox(Transition transition)
     {
-        Vector3 centralTransitionPoint = Vector3.zero;
-        float height = 0.1f;
-        float witdh = 0.1f;
-
-        switch (transition.transitionSide)
-        {
-            case TransitionSide.Left:
-                height = (worldTopRightLimit.y - worldBottomLeftLimit.y);
-                centralTransitionPoint += new Vector3(worldBottomLeftLimit.x, (worldTopRightLimit.y + worldBottomLeftLimit.y) / 2);
-                break;
-
-            case TransitionSide.Right:
-                height = (worldTopRightLimit.y - worldBottomLeftLimit.y);
-                centralTransitionPoint += new Vector3(worldTopRightLimit.x, (worldTopRightLimit.y + worldBottomLeftLimit.y) / 2);
-                break;
-
-            case TransitionSide.Bottom:
-                witdh = worldTopRightLimit.x - worldBottomLeftLimit.x;
-                centralTransitionPoint += new Vector3((worldTopRightLimit.x + worldBottomLeftLimit.x) / 2, worldBottomLeftLimit.y);
-                break;
-
-            case TransitionSide.Top:
-                witdh = worldTopRightLimit.x - worldBottomLeftLimit.x;
-                centralTransitionPoint += new Vector3((worldTopRightLimit.x + worldBottomLeftLimit.x) / 2, worldTopRightLimit.y);
-                break;
-
-        }
-
         var transitionGO = Instantiate(roomTransitionPrefab, transform);
-        transitionGO.transform.localPosition = centralTransitionPoint;
+        transitionGO.transform.position = GetColliderCentralPoint(transition);
         BoxCollider2D transitionCollider = transitionGO.GetComponent<BoxCollider2D>();
 
         transitionGO.SetData(transition.transitionSide, transition.newRoomID);
 
         transitionCollider.isTrigger = true;
-        transitionCollider.size = new Vector2(witdh, height);
+        transitionCollider.size = GetColliderHeightWidth(transition);
         transitionCollider.enabled = false;
 
         roomTransitions.Add(transitionGO);
     }
+
     public void SetRoomActive(bool active)
     {
         isRoomActive = active;
@@ -141,6 +120,95 @@ public class Room : MonoBehaviour
         if (isBottomLeft) return new Vector3(-gridX / 2 + (gridX * position.x), -gridY / 2 + (gridY * position.y), 0);
         else return new Vector3(gridX / 2 + (gridX * position.x), gridY / 2 + (gridY * position.y), 0);
     }
+
+
+    public Vector3 GetColliderCentralPoint(Transition transition)
+    {
+        Vector3 centralTransitionPoint = Vector3.zero;
+        float offset = 0f;
+        
+        if(transition.isColliderReduced) offset = GetOffset(transition);
+
+        switch (transition.transitionSide)
+        {
+            case TransitionSide.Left:
+                centralTransitionPoint += new Vector3(worldBottomLeftLimit.x, (worldTopRightLimit.y + worldBottomLeftLimit.y) / 2 + offset);
+                break;
+
+            case TransitionSide.Right:
+                centralTransitionPoint += new Vector3(worldTopRightLimit.x, (worldTopRightLimit.y + worldBottomLeftLimit.y) / 2 + offset);
+                break;
+
+            case TransitionSide.Bottom:
+                centralTransitionPoint += new Vector3((worldTopRightLimit.x + worldBottomLeftLimit.x) / 2 + offset, worldBottomLeftLimit.y);
+                break;
+
+            case TransitionSide.Top:
+                centralTransitionPoint += new Vector3((worldTopRightLimit.x + worldBottomLeftLimit.x) / 2 + offset, worldTopRightLimit.y);
+                break;
+
+        }
+
+        return centralTransitionPoint + transform.position;
+    }
+
+    private float GetOffset(Transition transition)
+    {
+        float offset = 0f;
+        float trueTransitionOffset = (transition.offset + 1) / 2; // From -1,1 to 0, 1
+
+        switch (transition.transitionSide)
+        {
+            case TransitionSide.Left:
+            case TransitionSide.Right:
+                var height = worldTopRightLimit.y - worldBottomLeftLimit.y;
+                offset = (height - (height * transition.size)) * 0.5f;
+                break;
+
+            case TransitionSide.Bottom:
+            case TransitionSide.Top:
+                var width = worldTopRightLimit.x - worldBottomLeftLimit.x;
+                offset = (width - (width * transition.size)) * 0.5f;
+                break;
+
+        }
+
+        Debug.Log($"{-offset} , {offset} , {trueTransitionOffset}");
+        return Mathf.Lerp(-offset , offset , trueTransitionOffset);
+
+    }
+
+    public Vector2 GetColliderHeightWidth(Transition transition)
+    {
+
+        float witdh = 0.1f;
+        float height = 0.1f;
+        float sizeMultiplicator = 1f;
+
+        if (transition.isColliderReduced) sizeMultiplicator = transition.size;
+
+        switch (transition.transitionSide)
+        {
+            case TransitionSide.Left:
+                height = (worldTopRightLimit.y - worldBottomLeftLimit.y) * sizeMultiplicator; 
+                break;
+
+            case TransitionSide.Right:
+                height = (worldTopRightLimit.y - worldBottomLeftLimit.y) * sizeMultiplicator;
+                break;
+
+            case TransitionSide.Bottom:
+                witdh = (worldTopRightLimit.x - worldBottomLeftLimit.x) * sizeMultiplicator;
+                break;
+
+            case TransitionSide.Top:
+                witdh = (worldTopRightLimit.x - worldBottomLeftLimit.x) * sizeMultiplicator;
+                break;
+
+        }
+
+        return new Vector2(witdh, height);
+    }
 }
 
 
@@ -149,5 +217,10 @@ public class Transition
 {
     public TransitionSide transitionSide;
     public int newRoomID;
+
+    public bool isColliderReduced;
+
+    [Range(-1f, 1f)] public float offset = 0f;
+    [Range(0.05f, 1)] public float size = 1f;
 
 }
